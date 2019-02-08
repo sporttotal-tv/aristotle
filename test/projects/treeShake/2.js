c45ca3646c61964a1afba715d1924a155fa75a80
Jim de Beer <beerdejim@gmail.com>
2019-02-08 09:53:33 +0100

    start of tree shake

---
+++ test/projects/treeShake/2.js
@@ -0,0 +1,7 @@
+const x = 'x'
+
+const y = 'y'
+
+const z = 'z'
+
+export { x, y, z }
---
+++ test/projects/treeShake/index.js
@@ -0,0 +1,16 @@
+import { x } from './2'
+
+// console.log('BLA', bla)
+// console.log('FLURPS', flurps)
+// console.log('FLOEPS', floeps)
+// console.log('FLAPS', flaps)
+// console.log('BLURF', blurf)
+// console.log('MURDER', murder)
+// console.log('STRANGER', stranger)
+
+// const blax = x.default
+
+// console.log(blax)
+// console.log(x.x.default)
+
+console.log(x)
