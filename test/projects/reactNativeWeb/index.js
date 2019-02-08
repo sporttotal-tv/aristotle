648b244d73b60c60bb7f1792ae299a8368739b04
Jim de Beer <beerdejim@gmail.com>
2019-02-08 15:19:26 +0100

    add tree shaking - specific compiles for r-native web

--- test/projects/reactNativeWeb/index.js
+++ test/projects/reactNativeWeb/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <View>{/* <Text>xx</Text> */}</View>
+  return (
+    <View>
+      <Text>xx</Text>
+    </View>
+  )
 }
 
 console.log('hello', View)
+
+ReactDOM.render(<App />, document.body)
 
 // AppRegistry.registerComponent('x', () => App)
 
--- test/server.js
+++ test/server.js
@@ -10,8 +10,7 @@ const fs = require('mz/fs')
 
 // store.noCache = true
 
-startServer(path.join(__dirname, './projects/reactStyle/index.js'), 3002)
-
+// startServer(path.join(__dirname, './projects/reactStyle/index.js'), 3002)
 // startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
-// production(path.join(__dirname, './projects/reactNativeWeb/index.js'), './dist')
+production(path.join(__dirname, './projects/reactNativeWeb/index.js'), './dist')
 // startServer(path.join(__dirname, './projects/treeShake/index.js'), 3002)
