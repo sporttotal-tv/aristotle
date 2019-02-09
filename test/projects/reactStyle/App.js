3aa12ef742cc4447cbf1cd551f4babe7aea42a75
Jim de Beer <beerdejim@gmail.com>
2019-02-09 17:50:14 +0100

    parse conditional

--- test/projects/reactStyle/App.js
+++ test/projects/reactStyle/App.js
@@ -2,22 +2,23 @@ import React, { useState } from 'react'
 import { Route, Switch, Provider } from 'hub'
 import hub from './hub'
 
-const Awesome = () => {
+const Awesome = ({ flap, snurf }) => {
   const [purple, setPurple] = useState(false)
 
   const bla = {
     border: '1px solid blue'
   }
 
+  console.log('xxx')
+
+  const flaps = purple ? 20 : 10
   return (
     <div
       onClick={() => setPurple(!purple)}
       style={{
-        background: purple ? 'purple' : 'blue',
-        padding: 30,
-        display: 'flex',
+        padding: flaps,
+        marginTop: purple ? 10 : flap ? 5 : snurf ? 1 : -10,
         ...bla
-        // transform: 'translate('
       }}
     >
       hahaha
