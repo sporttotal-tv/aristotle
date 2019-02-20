788a5f60398cc5e6eebe0793ba191e574ccb4f96
Jim de Beer <beerdejim@gmail.com>
2019-02-20 10:08:08 +0100

    use console.warn (not warning...) start of events browser module

---
+++ test/projects/events/index.js
@@ -0,0 +1,3 @@
+const events = require('events')
+
+console.log('snurpy', events)
--- test/server.js
+++ test/server.js
@@ -12,6 +12,7 @@ const fs = require('mz/fs')
 
 // process.env.NODE_ENV = 'production'
 // startServer(path.join(__dirname, './projects/reactStyle/index.js'), 3002)
-startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
+// startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
 // production(path.join(__dirname, './projects/reactNativeWeb/index.js'), './dist')
 // startServer(path.join(__dirname, './projects/treeShake/index.js'), 3002)
+startServer(path.join(__dirname, './projects/events/index.js'), 3002)
