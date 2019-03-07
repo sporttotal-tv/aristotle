39b0196080348f6e03e535bcda343984e1d58e5b
Jim de Beer <beerdejim@gmail.com>
2019-03-07 15:26:26 +0100

    dirty treeshake solution

--- test/server.js
+++ test/server.js
@@ -10,7 +10,7 @@ const fs = require('mz/fs')
 
 // store.noCache = true
 
-// process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = 'production'
 startServer(path.join(__dirname, './projects/reactStyle/index.js'), 3002)
 // startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
 // production(path.join(__dirname, './projects/reactNativeWeb/index.js'), './dist')
