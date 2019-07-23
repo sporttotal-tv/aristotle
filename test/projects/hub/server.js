2cbc7977b5fe97b79e202dc8b42e4762bd7e8c5c
Jim de Beer <beerdejim@gmail.com>
2019-07-23 16:02:13 +0200

    extra fix for start / end

--- test/projects/hub/server.js
+++ test/projects/hub/server.js
@@ -1,7 +1,10 @@
 // import { createClient } from '@saulx/hub'
 
+const fs = require('fs')
+
 // console.log('ssr go go go', createClient)
 
-export default async () => {
-  return 'flapxxxxxxxxxxxxxxxdrol'
+export default async (req, files, ua) => {
+  console.log(req.url, ua)
+  return 'flurkenstrut ------- x'
 }
