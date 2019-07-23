49099b6b6e3cf5c4da826c70cee971d952a6ccfa
Jim de Beer <beerdejim@gmail.com>
2019-07-23 16:45:12 +0200

    more advanced parsing

--- test/projects/hub/server.js
+++ test/projects/hub/server.js
@@ -1,10 +1,9 @@
-// import { createClient } from '@saulx/hub'
+import { createClient } from '@saulx/hub'
 
 const fs = require('fs')
 
 // console.log('ssr go go go', createClient)
-
+console.log('SERVER go go')
 export default async (req, files, ua) => {
-  console.log(req.url, ua)
-  return 'flurkenstrut ------- x'
+  return 'flurkenstrut -xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx------ x'
 }
