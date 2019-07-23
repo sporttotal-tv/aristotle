abd2efb717f560a04f76a4079d6240e0549228d8
Jim de Beer <beerdejim@gmail.com>
2019-07-23 16:58:10 +0200

    hub

--- test/projects/hub/server.js
+++ test/projects/hub/server.js
@@ -2,8 +2,10 @@ import { createClient } from '@saulx/hub'
 
 const fs = require('fs')
 
+const hub = createClient()
+
 // console.log('ssr go go go', createClient)
-console.log('SERVER go go')
+console.log('SERVER go go', hub)
 export default async (req, files, ua) => {
   return '<html>flurkenstxxxrsxxxdpofjdp iofhw------ </html>'
 }
