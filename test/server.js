ec42e69c22190b8ec96cfda3a76a691ffc08ef61
Jim de Beer <beerdejim@gmail.com>
2019-07-24 13:02:41 +0200

    native modiles, cache folder

--- test/server.js
+++ test/server.js
@@ -8,7 +8,7 @@ const {
 } = require('../lib')
 // const fs = require('mz/fs')
 
-store.noCache = true
+// store.noCache = true
 
 // process.env.NODE_ENV = 'production'
 // production(path.join(__dirname, '../../v2/apps/app/index.js'), './dist')
@@ -16,9 +16,9 @@ store.noCache = true
 // startServer(, 3002)
 // startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
 // startServer(path.join(__dirname, './projects/playground/index.js'), 3002)
-// production(path.join(__dirname, './projects/hub/index.js'), './dist')
+production(path.join(__dirname, './projects/hub/index.js'), './dist')
 
-startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
+// startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
 
 // also check murmur hash after this
 
