7ece4db5e964219ba91a69850be6fc6c5c0257b8
Jim de Beer <beerdejim@gmail.com>
2019-07-24 16:55:24 +0200

    multicore processing

--- test/projects/hub/server.js
+++ test/projects/hub/server.js
@@ -1,13 +1,3 @@
-// import { createClient } from '@saulx/hub'
-// const fs = require('fs')
-// const hub = createClient()
-// ;xw!@#
-
-global.ballz = true
-
-console.log('xxxxxxxxxxxxxxxx', global, typeof ballz)
-
-// console.log('ssr go go go', createClient)
 export default async (req, files, ua) => {
   return '<html>flurkenstxxxrsxxxdpofjdp iofhw------ </html>'
 }
