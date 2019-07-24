3f44564ac8289f6bc5258a9a9001aab9c54cb69c
Jim de Beer <beerdejim@gmail.com>
2019-07-24 17:20:48 +0200

    start of worker parsing

--- test/server.js
+++ test/server.js
@@ -8,17 +8,17 @@ const {
 } = require('../lib')
 // const fs = require('mz/fs')
 
-// store.noCache = true
+store.noCache = true
 
 // process.env.NODE_ENV = 'production'
 // production(path.join(__dirname, '../../v2/apps/app/index.js'), './dist')
 
 // startServer(, 3002)
-// startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
+startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
 // startServer(path.join(__dirname, './projects/playground/index.js'), 3002)
 // production(path.join(__dirname, './projects/hub/index.js'), './dist')
 
-startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
+// startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
 
 // also check murmur hash after this
 
