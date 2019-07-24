e66617a3057b1fc667d7632d28b21f4580862e92
Jim de Beer <beerdejim@gmail.com>
2019-07-24 14:22:49 +0200

    just use eval vm is too bad at handling global object the same way as node

--- test/projects/hub/server.js
+++ test/projects/hub/server.js
@@ -1,7 +1,12 @@
-import { createClient } from '@saulx/hub'
+// import { createClient } from '@saulx/hub'
 // const fs = require('fs')
-const hub = createClient()
+// const hub = createClient()
 // ;xw!@#
+
+global.ballz = true
+
+console.log('xxxxxxxxxxxxxxxx', global, typeof ballz)
+
 // console.log('ssr go go go', createClient)
 export default async (req, files, ua) => {
   return '<html>flurkenstxxxrsxxxdpofjdp iofhw------ </html>'
--- test/server.js
+++ test/server.js
@@ -16,9 +16,9 @@ const {
 // startServer(, 3002)
 // startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
 // startServer(path.join(__dirname, './projects/playground/index.js'), 3002)
-production(path.join(__dirname, './projects/hub/index.js'), './dist')
+// production(path.join(__dirname, './projects/hub/index.js'), './dist')
 
-// startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
+startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
 
 // also check murmur hash after this
 
