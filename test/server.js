cd9079227cc5c04c03878f42d8b1c74d07b42fa0
Jim de Beer <beerdejim@gmail.com>
2019-08-26 13:20:47 +0200

    better ssr process on dev

--- test/server.js
+++ test/server.js
@@ -1,31 +1,6 @@
 const path = require('path')
-const {
-  startServer,
-  store,
-  // build,
-  // buildProduction,
-  production
-} = require('../lib')
-// const fs = require('mz/fs')
+const { startServer } = require('../lib')
 
-store.noCache = true
+// store.noCache = true
 
-// process.env.NODE_ENV = 'production'
-// production(path.join(__dirname, '../../v2/apps/app/index.js'), './dist')
-
-// startServer(, 3002)
-// startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
-startServer(path.join(__dirname, './projects/playground/index.js'), 3002)
-// production(path.join(__dirname, './projects/hub/index.js'), './dist')
-
-// startServer(path.join(__dirname, './projects/hub/index.js'), 3002)
-
-// also check murmur hash after this
-
-// startServer(path.join(__dirname, './projects/sectionList/index.js'), 3002)
-// startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
-// production(path.join(__dirname, './projects/reactStyle/index.js'), './dist')
-// startServer(path.join(__dirname, './projects/treeShake/index.js'), 3002)
-// startServer(path.join(__dirname, './projects/events/index.js'), 3002)
-// startServer(path.join(__dirname, './projects/color/index.js'), 3002)
-// startServer(path.join(__dirname, './projects/color/index.js'), 3002)
+startServer(path.join(__dirname, './projects/simpleServer/index.js'), 3002)
