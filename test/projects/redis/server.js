681e531a6e74ed4a4f74411def1a5a31387859e0
Jim de Beer <beerdejim@gmail.com>
2019-10-11 16:07:46 +0200

    good ssr error handling

--- test/projects/redis/server.js
+++ test/projects/redis/server.js
@@ -1,5 +1,11 @@
 const redis = require('redis')
-const hub = require('@saulx/hub')
-console.info('flapper de flap', !!redis, !!hub)
-// console.log(!!redis)
+// const hub = require('@saulx/hub')
+// console.info('flapper de flap', !!redis, !!hub)
+console.log(!!redis)
+const flapflap = () => {
+  console.log('smurky', xxx)
+}
+
+flapflap()
+
 export default () => {}
