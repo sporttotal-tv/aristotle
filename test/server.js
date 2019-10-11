5fe0a5a00440ddbdfb46e3d7328dd9f41f83f18b
Jim de Beer <beerdejim@gmail.com>
2019-10-11 15:33:53 +0200

    fix loop in initlization errors

--- test/server.js
+++ test/server.js
@@ -8,14 +8,13 @@ store.noCache = true
 
 // startServer(, 3002)
 // startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
-// startServer(path.join(__dirname, './projects/playground/index.js'), 3002)
+startServer(path.join(__dirname, './projects/redis/index.js'), 3002)
 // production(path.join(__dirname, './projects/x/index.js'), './dist')
 // startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
 
-production(path.join(__dirname, './projects/redis/index.js'), './dist')
+// production(path.join(__dirname, './projects/redis/index.js'), './dist')
 
 // also check murmur hash after this
-
 // startServer(path.join(__dirname, './projects/sectionList/index.js'), 3002)
 // production(path.join(__dirname, './projects/reactStyle/index.js'), './dist')
 // startServer(path.join(__dirname, './projects/treeShake/index.js'), 3002)
