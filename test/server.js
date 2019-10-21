6ff19dbd89ee9940cfe7f4847315081e1b5af48c
Jim de Beer <beerdejim@gmail.com>
2019-10-21 12:00:31 +0200

    serve root files

--- test/server.js
+++ test/server.js
@@ -8,8 +8,8 @@ const { startServer, store, production } = require('../lib')
 
 // startServer(, 3002)
 // startServer(path.join(__dirname, '../../v2/apps/app/index.js'), 3002)
-startServer(path.join(__dirname, './projects/redis/index.js'), 3002)
-// production(path.join(__dirname, './projects/x/index.js'), './dist')
+// startServer(path.join(__dirname, './projects/redis/index.js'), 3002)
+production(path.join(__dirname, './projects/redis/index.js'), './dist')
 // startServer(path.join(__dirname, './projects/reactNativeWeb/index.js'), 3002)
 
 // production(path.join(__dirname, './projects/dynamicImports/index.js'), './dist')
