0d5be0c62d1bc78e672dcce89f27a54c6271d4bf
youzi <youri@saulx.com>
2021-01-15 09:38:32 +0100

    update build test

--- packages/build/test/build/index.ts
+++ packages/build/test/build/index.ts
@@ -3,19 +3,13 @@ import build from '../../src'
 import { join } from 'path'
 
 test('build', async t => {
-  const r = await build({
+  const { files, dependencies } = await build({
     entryPoints: [join(__dirname, 'app.tsx')],
     platform: 'node',
     external: ['redis'],
     sourcemap: true
-    // gzip: true,
-    // minify: true,
-    // splitting: true,
-    // format: 'esm'
   })
-  console.log(
-    '-->',
-    r.js[0].text.split('\n').find(v => /sourceMappingURL/.test(v))
-  )
-  console.log('files:', Object.keys(r.files))
+
+  t.truthy(dependencies.redis)
+  t.is(Object.keys(files).length, 5)
 })
