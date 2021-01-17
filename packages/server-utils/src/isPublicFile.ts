e4c0a308ea73b9e549e4fb0ca4b0896b9b95884f
Jim de Beer <beerdejim@gmail.com>
2021-01-17 08:44:10 +0100

    remove error

--- packages/server-utils/src/isPublicFile.ts
+++ packages/server-utils/src/isPublicFile.ts
@@ -1,6 +1,5 @@
 import { File } from '@saulx/aristotle-build'
 
 export default (file: File): boolean => {
-  return  file.mime.split('/')[0] !== 'application' &&
-    file.mime !== 'text/css'§
+  return file.mime.split('/')[0] !== 'application' && file.mime !== 'text/css'
 }
