3fc0a3cb8432fd58137e006c099704f009d80a1d
youzi <youri@saulx.com>
2021-01-19 10:48:33 +0100

    update versions script

---
+++ update-versions.js
@@ -0,0 +1,66 @@
+const { readFile, readdir, writeFile } = require('fs').promises
+const { join } = require('path')
+const { exec } = require('child_process')
+const { promisify } = require('util')
+const execAsync = promisify(exec)
+
+const PACKAGES = 'packages'
+
+readdir(PACKAGES).then(async files => {
+  const updated = {}
+  const pkgs = await Promise.all(
+    files.map(async f => {
+      const pkgDir = join(__dirname, PACKAGES, f)
+      const pkgPath = join(pkgDir, 'package.json')
+      const pkgText = await readFile(pkgPath)
+
+      const pkg = JSON.parse(pkgText)
+      const { stdout } = await execAsync('npm v --json', { cwd: pkgDir })
+      const npm = JSON.parse(stdout)
+
+      if (npm.version !== pkg.version) {
+        updated[pkg.name] = pkg
+      }
+
+      return { pkg, pkgPath }
+    })
+  )
+
+  let done
+  while (!done) {
+    done = true
+    await Promise.all(
+      pkgs.map(p => {
+        const { pkg, pkgPath, bumpedVersion } = p
+        if (pkg.dependencies) {
+          for (const depName in pkg.dependencies) {
+            if (depName in updated) {
+              const depVersion = pkg.dependencies[depName]
+              const newVersion = `^${updated[depName].version}`
+
+              if (newVersion !== depVersion) {
+                console.info(`Updating: ${pkgPath}:`)
+                console.info(`- ${depName} from ${depVersion} to ${newVersion}`)
+                if (!bumpedVersion) {
+                  const split = pkg.version.split('.')
+                  split[split.length - 1] = Number(split[split.length - 1]) + 1
+                  const bumpedVersion = split.join('.')
+                  console.info(
+                    `- version from ${pkg.version} to ${bumpedVersion}`
+                  )
+                  pkg.version = bumpedVersion
+                  p.bumped.version = true
+                }
+
+                pkg.dependencies[depName] = newVersion
+
+                done = false
+                return writeFile(pkgPath, JSON.stringify(pkg, null, 2))
+              }
+            }
+          }
+        }
+      })
+    )
+  }
+})
