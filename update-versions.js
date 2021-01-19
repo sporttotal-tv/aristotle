b44f109db8bc74f6f214b1573ef48b2cc03cdcd7
youzi <youri@saulx.com>
2021-01-19 10:55:35 +0100

    update versions script

--- update-versions.js
+++ update-versions.js
@@ -29,38 +29,42 @@ readdir(PACKAGES).then(async files => {
   let done
   while (!done) {
     done = true
-    await Promise.all(
-      pkgs.map(p => {
-        const { pkg, pkgPath, bumpedVersion } = p
-        if (pkg.dependencies) {
-          for (const depName in pkg.dependencies) {
-            if (depName in updated) {
-              const depVersion = pkg.dependencies[depName]
-              const newVersion = `^${updated[depName].version}`
-
-              if (newVersion !== depVersion) {
-                console.info(`Updating: ${pkgPath}:`)
-                console.info(`- ${depName} from ${depVersion} to ${newVersion}`)
-                if (!bumpedVersion) {
-                  const split = pkg.version.split('.')
-                  split[split.length - 1] = Number(split[split.length - 1]) + 1
-                  const bumpedVersion = split.join('.')
-                  console.info(
-                    `- version from ${pkg.version} to ${bumpedVersion}`
-                  )
-                  pkg.version = bumpedVersion
-                  p.bumpedVersion = true
-                }
-
-                pkg.dependencies[depName] = newVersion
-
-                done = false
-                return writeFile(pkgPath, JSON.stringify(pkg, null, 2))
+    for (const p of pkgs) {
+      const { pkg, pkgPath, bumpedVersion } = p
+      if (pkg.dependencies) {
+        for (const depName in pkg.dependencies) {
+          if (depName in updated) {
+            const depVersion = pkg.dependencies[depName]
+            const newVersion = `^${updated[depName].version}`
+            if (newVersion !== depVersion) {
+              console.info(`Updating: ${pkgPath}:`)
+              console.info(`- ${depName} from ${depVersion} to ${newVersion}`)
+              if (!bumpedVersion) {
+                const split = pkg.version.split('.')
+                split[split.length - 1] = Number(split[split.length - 1]) + 1
+                const bumpedVersion = split.join('.')
+                console.info(
+                  `- version from ${pkg.version} to ${bumpedVersion}`
+                )
+                pkg.version = bumpedVersion
+                p.bumpedVersion = true
               }
+              pkg.dependencies[depName] = newVersion
+
+              done = false
             }
           }
         }
-      })
-    )
+      }
+    }
   }
+
+  return Promise.all(
+    pkgs.map(({ pkg, pkgPath, bumpedVersion }) => {
+      if (bumpedVersion) {
+        console.info('write package:', pkgPath)
+        return writeFile(pkgPath, JSON.stringify(pkg, null, 2))
+      }
+    })
+  )
 })
